fdb8162f514d9b5a118eff2b3a526089c9ef3519
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2024-04-03 16:03:00 +0200

    Init

---
+++ hello-http/index.js
@@ -0,0 +1,20 @@
+// Simple http server that returns a JSON object with the following properties
+
+const http = require('http')
+
+var VERSION = 'V1'
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify({ message: "Hello World: " + VERSION }))
+}
+)
+
+process.on('SIGINT', function () {
+    process.exit();
+});
+
+server.listen(8080, () => {
+    console.log('Server running at http://localhost:8080/')
+})
+
